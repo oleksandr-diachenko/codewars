6892d43b022ad1d103c778bcaa48b325ba90aaec
oleksandr-diachenko <mail.positiv@gmail.com>
2020-09-14 17:29:53 +0300

    PaginationHelper

--- src/main/java/oleksandrdiachenko/kyu5/PigLatin/PigLatin.java
+++ src/main/java/oleksandrdiachenko/kyu5/PigLatin/PigLatin.java
@@ -1,5 +1,8 @@
 package oleksandrdiachenko.kyu5.PigLatin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author : Oleksandr Diachenko
  * @since : 9/14/2020
@@ -7,6 +10,33 @@ package oleksandrdiachenko.kyu5.PigLatin;
 public class PigLatin {
 
     public static String pigIt(String str) {
-        return "";
+        if (str.isEmpty()) {
+            return "";
+        }
+        List<String> reversedWords = new ArrayList<>();
+        for (String word : str.split(" ")) {
+            if (Character.isLetter(word.charAt(0))) {
+                reversedWords.add(swapWord(word));
+            } else {
+                reversedWords.add(word);
+            }
+        }
+        return String.join(" ", reversedWords);
+    }
+
+    private static String swapWord(String str) {
+        char[] chars = str.toCharArray();
+        for (int index = 0; index < chars.length - 1; index++) {
+            swap(chars, index);
+        }
+
+        return String.valueOf(chars) + "ay";
+    }
+
+    private static void swap(char[] chars, int index) {
+        char current = chars[index];
+        char next = chars[index + 1];
+        chars[index + 1] = current;
+        chars[index] = next;
     }
 }
--- src/test/java/oleksandrdiachenko/kyu5/PigLatin/PigLatinTest.java
+++ src/test/java/oleksandrdiachenko/kyu5/PigLatin/PigLatinTest.java
@@ -16,4 +16,32 @@ class PigLatinTest {
 
         assertThat(actual).isEmpty();
     }
+
+    @Test
+    void shouldReturn_Aay_When_A_Passed() {
+        String actual = PigLatin.pigIt("A");
+
+        assertThat(actual).isEqualTo("Aay");
+    }
+
+    @Test
+    void shouldReturn_BAay_When_A_Passed() {
+        String actual = PigLatin.pigIt("AB");
+
+        assertThat(actual).isEqualTo("BAay");
+    }
+
+    @Test
+    void shouldReturn_Aay_Bay_When_A_B_Passed() {
+        String actual = PigLatin.pigIt("A B");
+
+        assertThat(actual).isEqualTo("Aay Bay");
+    }
+
+    @Test
+    void shouldReturn_Aay_1_When_A_1_Passed() {
+        String actual = PigLatin.pigIt("A !");
+
+        assertThat(actual).isEqualTo("Aay !");
+    }
 }
